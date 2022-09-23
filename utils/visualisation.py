2f26eb0f380f1405b9aad00e58944ec7505767d2
Dmitriy Vahrushev <dv_test@github.com>
2022-09-24 00:45:42 +0300

    remove utils

--- utils/visualisation.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-
-def place_info_on_frame(frame, textlines:list, x_start=10, y_start=70):
-    i_increment = 30
-    for i, line in enumerate(textlines):
-        y = i*i_increment + y_start
-        cv2.putText(img=frame, text=line, org=(x_start, y), 
-            fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 255, 0),thickness=2)
