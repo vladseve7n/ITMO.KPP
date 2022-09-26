4782a14ac6e674e8225ec70fd5fa9d130cb975de
VladislavGmyr <vladgmyr98@gmail.com@>
2022-09-26 22:14:56 +0700

    Change: visualization

--- main.py
+++ main.py
@@ -1,67 +1,95 @@
-import os
 import argparse
-import shutil
+import os
+from typing import Union
+
 import cv2
+from pydantic import BaseModel
+
 from car_detection import CarDetector
 from plate_number_detection import PlateNumberDetector
-from checkpoint_passing import is_checkpoint_passed, is_plate_number_allowed
-from util.video import convert_images_to_video
-from util.visualisation import draw_textlines, draw_checkpoint_line, draw_rectangles, draw_plate_polygon
+from util.visualisation import draw_info_table, draw_checkpoint_line, draw_rectangles, draw_plate_polygon, \
+    draw_interested_area
+
+
+class FrameInfo(BaseModel):
+    car_colour: Union[str, None] = None
+    car_type: Union[str, None] = None
+    car_plate: Union[str, None] = None
+    is_car_passed: bool = False
 
 
 def main():
     parser = argparse.ArgumentParser(description='Process inputs.')
     parser.add_argument('--videos', type=str,
-                    help='path to videos for inference', default='test_videos/test_video_cars.mp4')
+                        help='path to videos for inference', default='test_videos/test_video_cars.mp4')
     parser.add_argument('--allowed_cars_info', type=str,
-                    help='path to file with info about allowed cars', default='test_allowed_cars_info.csv')
+                        help='path to file with info about allowed cars', default='test_allowed_cars_info.csv')
     parser.add_argument('--output_path', type=str,
-                    help='path to output folder', default='output')
+                        help='path to output folder', default='output')
     args = parser.parse_args()
     video_paths = args.videos.split(',')
     output_path = args.output_path
     allowed_cars_info_path = args.allowed_cars_info
+
     # Open video
-    cap = cv2.VideoCapture(video_paths[0]) # TODO: add support for multiple video/camera processing 
+    cap = cv2.VideoCapture(video_paths[0])  # TODO: add support for multiple video/camera processing
     if not cap.isOpened():
         print("Error opening video stream or file")
-    TEMP_FOLDER_PATH = 'temp'
-    os.makedirs(TEMP_FOLDER_PATH, exist_ok=True)
     os.makedirs(output_path, exist_ok=True)
-    CHECKPOINT_LINE_COORDS = [(150, 0), (150, 720)] # TODO: ask for these coords as cli args
 
+    CHECKPOINT_LINE_COORDS = [(150, 0), (150, 720)]  # TODO: ask for these coords as cli args
+    INTERESTED_AREA = [(100, 0), (600, 720)]
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     out = cv2.VideoWriter(output_path + '/output.avi',
-                          cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
+                          cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width + 400, frame_height))
 
     car_detector = CarDetector()
     plate_number_detector = PlateNumberDetector()
 
-    frame_num = 0
-    while(cap.isOpened()):
+    while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
-            break 
-        # 1. Detect cars on the frame
+            break
+            # 1. Detect cars on the frame
+        print('.', end='')
+        frame_info = FrameInfo()
+        res_frame = frame.copy()
+        frame = frame[INTERESTED_AREA[0][1]:INTERESTED_AREA[1][1], INTERESTED_AREA[0][0]:INTERESTED_AREA[1][0], :]
+
         car_bboxes = car_detector.detect(frame)
-        plate_polygon = plate_number_detector.detect(frame)
+        if len(car_bboxes) == 0:
+            res_frame = draw_info_table(res_frame, frame_info)
+            draw_checkpoint_line(res_frame, CHECKPOINT_LINE_COORDS)
+            draw_interested_area(res_frame, INTERESTED_AREA)
+            out.write(res_frame)
+            continue
+        else:
+            frame_info.car_colour = 'Undefined'
+            frame_info.car_type = 'Undefined'
         # 2. Detect if car passed the checkpoint line
-        # 3. crop car boundin  g box
+        # 3. crop car bounding box
         # 4. Detect plate number of the nearest car
-        # Use some standard library like Nomeroff
+        plate_polygon = plate_number_detector.detect(frame)
+        if plate_polygon is not None:
+            frame_info.car_plate = "Undefined"
         # 5. Open the gate if plate number in the list of allowed number plates.
         # Or close the gate if there are no allowed cars nearby
         # 6. Visualisation
-        res_frame = frame.copy()
-        draw_checkpoint_line(res_frame, CHECKPOINT_LINE_COORDS)
-        draw_rectangles(res_frame, car_bboxes)
-        draw_plate_polygon(res_frame, plate_polygon)
 
-        draw_textlines(res_frame, ['Car passed', 'Checkpoint opened'])
-        cv2.imshow('test', res_frame)
-        cv2.waitKey(0)
+        draw_rectangles(frame, car_bboxes)
+        draw_plate_polygon(frame, plate_polygon)
+
+        res_frame[INTERESTED_AREA[0][1]:INTERESTED_AREA[1][1], INTERESTED_AREA[0][0]:INTERESTED_AREA[1][0], :] = frame
+
+
+        draw_checkpoint_line(res_frame, CHECKPOINT_LINE_COORDS)
+        draw_interested_area(res_frame, INTERESTED_AREA)
+        res_frame = draw_info_table(res_frame, frame_info)
         out.write(res_frame)
+        # cv2.imshow('test', res_frame)
+        # cv2.waitKey(0)
+
 
     cap.release()
     cv2.destroyAllWindows()
--- util/visualisation.py
+++ util/visualisation.py
@@ -1,12 +1,26 @@
 import cv2
+import numpy as np
 
 
-def draw_textlines(frame, textlines: list, x_start=10, y_start=70):
-    i_increment = 35
-    for i, line in enumerate(textlines):
-        y = i * i_increment + y_start
-        cv2.putText(img=frame, text=line, org=(x_start, y),
-                    fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(0, 255, 0), thickness=2)
+def draw_info_table(frame, frame_info) -> np.array:
+    info_table = np.zeros((frame.shape[0], 400, 3), dtype='uint8')
+
+    cv2.putText(img=info_table, text=f'Car colour: {frame_info.car_colour}', org=(5, 50),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(255, 255, 255), thickness=2)
+
+    cv2.putText(img=info_table, text=f'Car type: {frame_info.car_type}', org=(5, 80),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(255, 255, 255), thickness=2)
+
+    cv2.putText(img=info_table, text=f'Car plate: {frame_info.car_plate}', org=(5, 110),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.7, color=(255, 255, 255), thickness=2)
+
+    frame = np.hstack((frame, info_table))
+    return frame
+
+
+def draw_interested_area(frame, INTERESTED_AREA):
+    cv2.rectangle(frame, (INTERESTED_AREA[0][0], INTERESTED_AREA[0][1]),
+                  (INTERESTED_AREA[1][0], INTERESTED_AREA[1][1]), (200, 120, 50), 2)
 
 
 def draw_checkpoint_line(frame, line_coords):
